79dc6babe2106ad592baf1e26e8ecf6ea1ac08bc
James <jacaudil@calpoly.edu>
2016-12-17 18:00:18 -0800

    Test mul on single tensor.

---
+++ testML1.py
@@ -0,0 +1,20 @@
+"""
+First machine learning program utilizing tensorflow
+Author: James Caudill
+
+leggo
+"""
+
+import tensorflow as tf
+
+x1 = tf.constant(5)
+x2 = tf.constant(6)
+
+result = tf.mul(x1, x2)
+
+print (result)
+
+with tf.Session() as sess:
+	print (sess.run(result))
+
+
